86c4221b2b0197f16a8c5a4081b24af76623d221
Оля <olyakorcha@gmail.com>
2023-03-09 16:49:34 +0300

    add Recipe model

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -2,7 +2,32 @@ from django.contrib import admin
 from recipes.models import Ingredient, Recipe, RecipeIngredient, Tag
 
 
+class RecipeIngredientInline(admin.TabularInline):
+    model = Recipe.ingredients.through
+    extra = 1
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'ingredient',
+        'recipe',
+        'amount',
+    )
+    empty_value_display = '-пусто-'
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'measurement_unit',
+    )
+    list_filter = ('name',)
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
 class RecipeAdmin(admin.ModelAdmin):
+    inlines = (RecipeIngredientInline,)
     list_display = (
         'name',
         'author',
@@ -19,42 +44,16 @@ class RecipeAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-class RecipeIngredientAdmin(admin.ModelAdmin):
-    list_display = (
-        'ingredient',
-        'recipe',
-        'amount',
-    )
-    empty_value_display = '-пусто-'
-
-
 class TagAdmin(admin.ModelAdmin):
     list_display = (
         'name',
         'color',
         'slug',
     )
-    list_filter = (
-        'name',
-    )
-    search_fields = (
-        'name',
-    )
+    list_filter = ('name',)
+    search_fields = ('name',)
     empty_value_display = '-пусто-'
 
-
-class IngredientAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'measurement_unit',
-    )
-    list_filter = (
-        'name',
-    )
-    search_fields = (
-        'name',
-    )
-    empty_value_display = '-пусто-'
 
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
